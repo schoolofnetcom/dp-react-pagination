d844b81665b7d6744c6a45d45c4e4d8c1080f208
Leonan Luppi <leonan.luppi@gmail.com>
2021-02-17 11:07:24 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [offset, setOffset] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+  const [curPage, setCurPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [curData, setCurData] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const fetchTodos = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await res.json();
+    setItems(data); //items
+    const slicedData = data.slice(offset, offset + perPage);
+    setPageCount(Math.ceil(data.length / perPage));
+    const curData = slicedData.map(i => <> <p key={i.id}>{i.title}</p>  </>)
+    setCurData(curData);
+  }
+
+  const handlePageClick = (event) => {
+    const { selected } = event;
+    const offset = selected * perPage;
+    setCurPage(selected);
+    setOffset(offset);
+    fetchTodos();
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  
+  return(
+    <div>
+      {curData}
+      <ReactPaginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 }
